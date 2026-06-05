f2d166010bbbd0ed00b2ad887d50a20143d33be9
varunsainani <varunsainani0990@gmail.com>
2026-06-05 14:10:41 +0000

    feat(models): add template sequelize model

---
+++ backend/src/models/Template.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    "Template",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      }
+    },
+    {
+      tableName: "templates",
+      timestamps: false
+    }
+  );
